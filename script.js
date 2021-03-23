33a682fb36ec77a027202be0b1ac133aee2b1b52
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-23 12:50:39 +0000

    Added notes to script.js.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let a = 0
 let b = 0
 let h = 0
 let answer = 0
-
+/* These variables above represent the variables in our equation. */ 
 document.getElementById('calculate').addEventListener('click', find)
-
+/* Once the user inserts the values into the input boxes, they will click the Calculate button. Then, the functions for this button will occur below. */ 
 function find () {
   a = document.getElementById('base-a').value
   a = parseInt(a)
@@ -12,6 +12,9 @@ function find () {
   b = parseInt(b)
   h = document.getElementById('height').value
   h = parseInt(h)
+  /* ParInt tells the program that whatever is inserted into the input boxes is a integer and not a symbol. */
   answer = (a + b) * h / 2
+  /* This is the equation that will calculate the area of the trapezoid. */
+  /* The lines of code above show the process that the computeris following to calculate the area using the variables. */
   alert(answer)
 }
